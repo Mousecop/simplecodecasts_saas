ab8e4b79b7ba014281b82ccf68e02a436526774a
Jake Askew <joaskew13@gmail.com>
2016-08-14 19:37:26 +0000

    Finally migrated!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719005914) do
+ActiveRecord::Schema.define(version: 20160814192712) do
 
   create_table "contacts", force: true do |t|
   end
