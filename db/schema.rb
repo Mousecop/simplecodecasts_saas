eb76a985ef9e949b90c992bb7c64e0193e7f9900
Jake Askew <joaskew13@gmail.com>
2016-08-15 14:18:57 +0000

    Created contact form view

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,11 @@
 ActiveRecord::Schema.define(version: 20160814192712) do
 
   create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
