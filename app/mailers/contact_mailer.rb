965c957c8eec1b9ab6385c186bc7836598e7e30b
Jake Askew <joaskew13@gmail.com>
2016-10-06 02:17:25 +0000

    added mailer and view.

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'joaskew13@gmail.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
